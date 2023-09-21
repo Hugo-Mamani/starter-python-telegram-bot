21a1afe3e1b350748f2b76130b5b7e125bfa19ef
Hugo Mamani <mamaniwilliam383@gmail.com>
2023-09-21 15:40:49 -0500

    build first version

--- bot.py
+++ bot.py
@@ -3,20 +3,19 @@ import telebot
 key = "6489296743:AAHNHhQirARtbb7HIHE6WQAeF_Prp5gsUtk"
 
 bot = telebot.TeleBot(key)
+bot.remove_webhook()  # No es necesario, ya que no estamos configurando una webhook
 
-@bot.message_handler(commands=["start","codes","love"])
+@bot.message_handler(commands=["start", "codes", "love"])
 def cmd(message):
     bot.reply_to(message, "hey, commands available")
 
 @bot.message_handler(content_types=["text"])
 def text(message):
     if message.text.startswith("/"):
-        bot.send_message(message.chat.id ,"sorry not exist this command")
+        bot.send_message(message.chat.id, "sorry, this command does not exist")
     else:
-        bot.send_message(message.chat.id, "hey, but details")
+        bot.send_message(message.chat.id, "hey, more details")
 
 if __name__ == "__main__":
     print('start bot')
-    bot.remove_webhook()
-    bot.set_webhook(url='https://netbot.cyclic.app/' + key)
     bot.polling(none_stop=True)
